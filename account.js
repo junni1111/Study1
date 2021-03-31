95177cab46d2892b5fd7a5553a1e1fdd08fc1a76
junni1111 <78162811+junni1111@users.noreply.github.com>
2021-03-31 23:09:13 +0900

    everybody

--- account.js
+++ account.js
@@ -20,6 +20,11 @@ function joinAccount(idText, pwText){
         id : idText,
         pw : pwText
     };
+
+
+
+
+
     accounts.forEach(function(account){
         if(account.id === idText) flag = false;
     });
